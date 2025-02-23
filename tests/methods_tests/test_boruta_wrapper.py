61dafae26f8a368d70e954b3a7a94759bede1dc3
Jakub Nurkiewicz <78982788+NUREKK2003@users.noreply.github.com>
2025-02-23 19:18:11 +0100

    Tests optimize (#18)
    * 8 times faster tests
    
    250 s -> 44 s (including 27 s inicialization)
    
    * Update .gitignore

--- tests/methods_tests/test_boruta_wrapper.py
+++ tests/methods_tests/test_boruta_wrapper.py
@@ -1,34 +1,14 @@
 from __future__ import annotations
 
-from typing import Callable
-
 import pandas as pd
 import pytest
 from sklearn.datasets import make_classification, make_regression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import train_test_split
 
 from dataclr.methods import BorutaMethod
 from dataclr.metrics.metrics import CLASSIFICATION_METRICS, REGRESSION_METRICS
 from dataclr.results import Result, ResultPerformance
-
-
-def generate_dataset(
-    dataset_generator: Callable,
-) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    X, y = dataset_generator(
-        n_samples=10000, n_features=10, n_informative=3, random_state=42
-    )
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-    return (
-        pd.DataFrame(X_train),
-        pd.DataFrame(X_test),
-        pd.Series(y_train),
-        pd.Series(y_test),
-    )
 
 
 @pytest.mark.parametrize(
@@ -42,7 +22,7 @@ def generate_dataset(
         ),
     ],
 )
-def test_ranked_features(dataset, model, metric):
+def test_ranked_features(generate_dataset, dataset, model, metric):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     model.fit(X_train, y_train)
     boruta = BorutaMethod(model=model, metric=metric, n_results=3, seed=42)
@@ -65,7 +45,9 @@ def test_ranked_features(dataset, model, metric):
         ),
     ],
 )
-def test_ranked_features_with_no_importance_model(dataset, model, metric):
+def test_ranked_features_with_no_importance_model(
+    generate_dataset, dataset, model, metric
+):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     model.fit(X_train, y_train)
     boruta = BorutaMethod(model=model, metric=metric, n_results=3, seed=42)
@@ -90,7 +72,7 @@ def test_ranked_features_with_no_importance_model(dataset, model, metric):
         ),
     ],
 )
-def test_results_list(dataset, model, metric):
+def test_results_list(generate_dataset, dataset, model, metric):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     boruta = BorutaMethod(model=model, metric=metric, n_results=3, seed=42)
     results = boruta.fit_transform(X_train, X_test, y_train, y_test)
@@ -117,7 +99,7 @@ def test_results_list(dataset, model, metric):
         ),
     ],
 )
-def test_result_performance(dataset, model, metric, metrics):
+def test_result_performance(generate_dataset, dataset, model, metric, metrics):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     boruta = BorutaMethod(model=model, metric=metric, n_results=3, seed=42)
     results = boruta.fit_transform(X_train, X_test, y_train, y_test)
--- tests/methods_tests/test_shap_wrapper.py
+++ tests/methods_tests/test_shap_wrapper.py
@@ -1,34 +1,14 @@
 from __future__ import annotations
 
-from typing import Callable
-
 import pandas as pd
 import pytest
 from sklearn.datasets import make_classification, make_regression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import train_test_split
 
 from dataclr.methods import ShapMethod
 from dataclr.metrics.metrics import CLASSIFICATION_METRICS, REGRESSION_METRICS
 from dataclr.results import Result, ResultPerformance
-
-
-def generate_dataset(
-    dataset_generator: Callable,
-) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    X, y = dataset_generator(
-        n_samples=10000, n_features=10, n_informative=3, random_state=42
-    )
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-    return (
-        pd.DataFrame(X_train),
-        pd.DataFrame(X_test),
-        pd.Series(y_train),
-        pd.Series(y_test),
-    )
 
 
 @pytest.mark.parametrize(
@@ -48,7 +28,7 @@ def generate_dataset(
         ),
     ],
 )
-def test_ranked_features(dataset, model, metric):
+def test_ranked_features(generate_dataset, dataset, model, metric):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     model.fit(X_train, y_train)
     shap = ShapMethod(model=model, metric=metric, n_results=3, seed=42)
@@ -71,7 +51,7 @@ def test_ranked_features(dataset, model, metric):
         ),
     ],
 )
-def test_results_list(dataset, model, metric):
+def test_results_list(generate_dataset, dataset, model, metric):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     shap = ShapMethod(model=model, metric=metric, n_results=3, seed=42)
     results = shap.fit_transform(X_train, X_test, y_train, y_test)
@@ -98,7 +78,7 @@ def test_results_list(dataset, model, metric):
         ),
     ],
 )
-def test_result_performance(dataset, model, metric, metrics):
+def test_result_performance(generate_dataset, dataset, model, metric, metrics):
     X_train, X_test, y_train, y_test = generate_dataset(dataset)
     shap = ShapMethod(model=model, metric=metric, n_results=3, seed=42)
     results = shap.fit_transform(X_train, X_test, y_train, y_test)
