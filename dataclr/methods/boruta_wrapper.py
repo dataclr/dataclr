f2210200b85c315980d1ef061261735e831213de
Jakub Nurkiewicz <78982788+NUREKK2003@users.noreply.github.com>
2025-03-02 15:45:47 +0100

    boruts docs

--- dataclr/methods/boruta_wrapper.py
+++ dataclr/methods/boruta_wrapper.py
@@ -67,6 +67,8 @@ class BorutaMethod(WrapperMethod):
             X_test (pd.DataFrame): Testing feature matrix.
             y_train (pd.Series): Training target variable.
             y_test (pd.Series): Testing target variable.
+            max_features (int): Number of max features count in results.
+            Default to -1 (all features number).
 
         Returns:
             list[Result]: List of results for the selected features.
